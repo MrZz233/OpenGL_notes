a6b26113b39ff465685d9aae115fc143cf20ce14
k_night_zz <619416187@qq.com>
2021-12-20 02:17:19 +0800

    空窗口

--- 1_Triangle/01空窗口/main.cpp
+++ 1_Triangle/01空窗口/main.cpp
@@ -1,17 +1,20 @@
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
-
 #include <iostream>
 
+//函数声明
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window)
 
 int main()
 {
+	//初始化glfw
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	//创建glfw窗口，并绑定至当前上下文
 	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
 	if (window == NULL)
 	{
@@ -21,24 +24,42 @@ int main()
 	}
 	glfwMakeContextCurrent(window);
 
+	//使用OpenGL函数之前，初始化glad
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
 
+	//设置窗口变化的回调函数
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
+	
+	//主循环
 	while (!glfwWindowShouldClose(window))
 	{
+		//处理按键事件
+		processInput(window);	
+		//渲染循环
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
 		glfwSwapBuffers(window);
+		//监听鼠标和按键发生
 		glfwPollEvents();
 	}
-
+	//结束glfw
+	glfwTerminate();
 	return 0;
 }
 
+//窗口大小调整函数
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
+}
+
+//按键处理函数
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
 }
